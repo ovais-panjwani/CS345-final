19ac66e9f73a8c03a56232efb6bb153c918686d6
Jesse Mendoza <elswampnuttz@yahoo.com>
2016-11-08 00:25:42 -0600

    just started the parser, added the scala.util.parser jar file to the buildpath

---
+++ SQL_DSL/src/sql_dsl/SQLParser.scala
@@ -0,0 +1,10 @@
+
+
+package sql_dsl
+
+import scala.util.parsing.combinator._
+import scala.util.parsing.combinator.syntactical._
+
+object SQLParser extends JavaTokenParsers{
+  
+}
---
+++ SQL_DSL/src/sql_dsl/SQLQuery.scala
@@ -0,0 +1,5 @@
+package sql_dsl
+
+object SQLQuery {
+  
+}
